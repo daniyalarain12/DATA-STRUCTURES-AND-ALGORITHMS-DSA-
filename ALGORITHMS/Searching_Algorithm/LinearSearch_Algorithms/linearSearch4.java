706ba6f157e1a01e284d9f13596154cec4eef93c
Daniyal <daniyalarain123786@gmail.com>
2025-08-27 01:33:05 +0500

    Update linearSearch4.java

--- ALGORITHMS/Searching_Algorithm/LinearSearch_Algorithms/linearSearch4.java
+++ ALGORITHMS/Searching_Algorithm/LinearSearch_Algorithms/linearSearch4.java
@@ -1,1 +1,20 @@
-
+public class linearSearch4 {
+    public static int thirdTargetIndex(String[] arr,String target,int times) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==target) {
+                count++;
+            }
+            if (count==times) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String[] arr = {"DANIYAL","UMAIR","UMAIR","MOHAMMAD ALI","UMAIR","SIR NAVEEN KUMAR","UMAIR"};
+        String target = "UMAIR";
+        int times = 3;
+        System.out.println("THIRD TIME TARGET EXIST AT INDEX : " + thirdTargetIndex(arr,target,times));
+    }
+}
