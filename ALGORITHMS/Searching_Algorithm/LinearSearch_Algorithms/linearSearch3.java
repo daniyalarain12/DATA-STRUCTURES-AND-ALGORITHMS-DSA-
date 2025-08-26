ce5a26974da9f98705adb6d3427561628fa804dd
Daniyal <daniyalarain123786@gmail.com>
2025-08-27 01:28:01 +0500

    Create linearSearch3.java

---
+++ ALGORITHMS/Searching_Algorithm/LinearSearch_Algorithms/linearSearch3.java
@@ -0,0 +1,20 @@
+public class linearSearch3 {
+    public static int thirdTargetIndex(String[] arr,String target,int times) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==target) {
+                count++;
+            }
+            if (count==times) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String[] arr = {"DANIYAL","UMAIR","UMAIR","MOHAMMAD ALI","UMAIR","SIR NAVEEN KUMAR","UMAIR"};
+        String target = "UMAIR";
+        int times = 3;
+        System.out.println("THIRD TIME TARGET EXIST AT INDEX : " + thirdTargetIndex(arr,target,times));
+    }
+}
