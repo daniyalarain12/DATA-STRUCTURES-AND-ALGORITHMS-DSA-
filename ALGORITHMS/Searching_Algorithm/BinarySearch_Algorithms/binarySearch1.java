ab73b2eb498b772b88f65f403a8c16cc957c8e47
Daniyal <daniyalarain123786@gmail.com>
2025-08-27 01:37:08 +0500

    Create binarySearch1.java

---
+++ ALGORITHMS/Searching_Algorithm/BinarySearch_Algorithms/binarySearch1.java
@@ -0,0 +1,23 @@
+// Binary Search Using Iterations
+public class binarySearch1 {
+    public static int binarySearch(int[] arr,int target) {
+        int start = 0 , end = arr.length-1;
+        while (start<=end) {
+            int mid = (start+end)/2;
+            //int mid = start + (end-start)/2;
+            if (arr[mid]==target) {
+                return mid;
+            } else if (arr[mid]<target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int target = 9;
+        System.out.println("TARGET " + target + " FOUND AT INDEX : " + binarySearch(arr,target));
+    }
+}
