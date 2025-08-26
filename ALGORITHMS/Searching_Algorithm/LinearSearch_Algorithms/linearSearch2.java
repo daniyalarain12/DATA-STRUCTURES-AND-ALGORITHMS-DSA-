a24a8d3c9885e2c722e267c28fb76dfb28a9c493
Daniyal <daniyalarain123786@gmail.com>
2025-08-27 01:31:47 +0500

    Update linearSearch2.java

--- ALGORITHMS/Searching_Algorithm/LinearSearch_Algorithms/linearSearch2.java
+++ ALGORITHMS/Searching_Algorithm/LinearSearch_Algorithms/linearSearch2.java
@@ -1,26 +1,15 @@
 public class linearSearch2 {
-    public static int[] linearSearch(String[] arr,String target) {
-        int[] dummy = new int[arr.length];
-        int count = 0;
+    public static int linearSearch(String[] arr,String target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].toLowerCase().contains(target)) {
-                dummy[count] = i;
-                count++;
+                return i;
             }
         }
-        int[] record = new int[count];
-        for (int i = 0; i < count; i++) {
-            record[i] = dummy[i];
-        }
-        return record;
+        return -1;
     }
     public static void main(String[] args) {
         String[] arr = {"DANIYAL","UMAIR","ali RAZA","MOHAMMAD ALI","SIR NAVEEN KUMAR"};
         String target = "ali";
-        int[] arr2 = linearSearch(arr,target);
-        System.out.print("TARGET IS PRESENT AT INDICES : ");
-        for (int i : arr2) {
-            System.out.print(i + " ");
-        }
+        System.out.println("TARGET IS PRESENT AT INDEX : " + linearSearch(arr,target));
     }
 }
